b3442870b9fc24564666b52cd85b1e1e71ecddf6
CLyieee <130021408+CLyieee@users.noreply.github.com>
2024-10-30 07:28:21 +0800

    Update orderService.js

--- a-app/src/app/services/orderService.js
+++ a-app/src/app/services/orderService.js
@@ -1,8 +1,8 @@
-// services/orderService.js
 import axios from "axios";
 
 const BASE_URL = "https://bigbew-service.vercel.app"; // Adjust the port as necessary
 
+// Create a new order
 const createOrder = async (orderData) => {
   try {
     const response = await axios.post(`${BASE_URL}/api/order/create`, orderData);
@@ -13,7 +13,7 @@ const createOrder = async (orderData) => {
   }
 };
 
-
+// Fetch all orders
 const getAllOrders = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/api/orders/get/all`);
@@ -23,19 +23,21 @@ const getAllOrders = async () => {
   }
 };
 
-// export const getAllOrders = async () => {
-//   try {
-//     const response = await axios.get('${BASE_URL}/api/order/get/all'); // Update to match your endpoint
-//     return response.data;
-//   } catch (error) {
-//     throw new Error('Error fetching orders: ' + error.message);
-//   }
-// };
+// Fetch an order by order number
+const getOrderByNumber = async (orderNumber) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/orders/get/all/${orderNumber}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching order: ${error.message}`);
+  }
+};
 
+// Update an order
 const updateOrder = async (orderNumber, updateData) => {
   try {
     const response = await axios.put(
-      `${BASE_URL}/api/orders/${orderNumber}`,
+      `${BASE_URL}/api/orders/update/${orderNumber}`,
       updateData
     );
     return response.data;
@@ -44,4 +46,4 @@ const updateOrder = async (orderNumber, updateData) => {
   }
 };
 
-export { createOrder, getAllOrders, updateOrder };
+export { createOrder, getAllOrders, getOrderByNumber, updateOrder };
