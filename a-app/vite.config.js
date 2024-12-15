e784069e0c7ecf0446306a30e2c1449bae722084
CLyieee <130021408+CLyieee@users.noreply.github.com>
2024-12-15 12:25:57 +0800

    Update vite.config.js

--- a-app/vite.config.js
+++ a-app/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'framer-motion': 'framer-motion/dist/framer-motion.esm.js', // Ensure the correct ESM build is used
+    },
+  },
+});
